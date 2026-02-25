346ab631614ec7ff526f1669e4dc1aa16e23acdf
flomartel <fjm25@kent.ac.uk>
2026-02-25 12:22:56 +0000

    validator_ver1

--- validator.py
+++ validator.py
@@ -1,5 +1,15 @@
 import re
 
 def is_valid_email(email):
-    if re.search(r"^\w+@\w.+\.(ac.uk|gov.uk|nhs.net)$", email)
-    
+    if re.search(r"^\w+@\w.+\.(ac.uk)$", email):
+        return True
+    return False
+
+def main():
+    email = input("What's your email? ").strip()
+    if is_valid_email(email):
+        print("Valid")
+    else:
+        print("Invalid")
+if __name__ == "__main__":
+    main()
