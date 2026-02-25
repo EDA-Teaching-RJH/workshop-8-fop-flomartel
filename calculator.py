cb3e0dec3efebe31025c39139920ecfe9d75fb28
flomartel <fjm25@kent.ac.uk>
2026-02-25 11:44:40 +0000

    calculator_ver1

---
+++ calculator.py
@@ -0,0 +1,11 @@
+#introduction to unit testing
+
+def main():
+    x = int(input("What's x? "))
+    print("x squared is", square(x))
+
+def square(n):
+    return n * n
+
+if __name__ == "__main__":
+    main()
