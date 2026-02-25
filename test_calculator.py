06158045fc1815d17f1cdc6c8c7415a393815950
flomartel <fjm25@kent.ac.uk>
2026-02-25 12:10:37 +0000

    ver3

--- test_calculator.py
+++ test_calculator.py
@@ -4,12 +4,9 @@ def main():
     test_square()
 
 def test_square():
-    if square(2) != 4:
-        print("2 squared was not 4")
-    else:
-        print("2 squared was 4, that's great")
+ assert square(2) == 4
+ assert square(3) == 9
 
-    if square(3) != 9:
-        print("3 squared was not 9")
+
 if __name__ == "__main__":
     main()
