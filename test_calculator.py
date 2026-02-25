d4cde9b002493a86b9f905a2167211fc967bfb9f
flomartel <fjm25@kent.ac.uk>
2026-02-25 12:17:33 +0000

    ver4

--- test_calculator.py
+++ test_calculator.py
@@ -1,12 +1,17 @@
 from calculator import square
 
 def main():
-    test_square()
-
+ test_square()
 def test_square():
- assert square(2) == 4
- assert square(3) == 9
+    try:
+        assert square(-2) == 4
+    except AssertionError:
+        print("-2 squared is not 4")
 
-
+    try:
+        assert square(0) == 0
+    except AssertionError:
+        print("0 squared is not 0")
+        
 if __name__ == "__main__":
     main()
---
+++ validator.py
@@ -0,0 +1,5 @@
+import re
+
+def is_valid_email(email):
+    if re.search(r"^\w+@\w.+\.(ac.uk|gov.uk|nhs.net)$", email)
+    
