4552b11bb970b2379750eb14765cf30cb10bf0fb
flomartel <fjm25@kent.ac.uk>
2026-02-25 11:31:20 +0000

    number_ver3

--- phone_numbers.py
+++ phone_numbers.py
@@ -1,6 +1,7 @@
 import re
+
 number = input("What is your phone number? ")
-if re.search("^07\d{9}$", number):
+if re.search("^07\d{9}$", number): #makes sure number starts with 07, is decimal digits, and has 9 digits after the 07
     print("Valid")
 else:
     print("Invalid")
